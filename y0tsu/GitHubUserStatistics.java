dd1e7a900aabb97b0bdc74cc0475d420ff91f289
y0tsu <maksimbytenko10@gmail.com>
2025-10-16 08:52:04 +0300

    Fix USER_NAME error in <y0tsu>/GitHubUserStatistics.java

--- y0tsu/GitHubUserStatistics.java
+++ y0tsu/GitHubUserStatistics.java
@@ -4,7 +4,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class GitHubUserStatistics {
-    public final static String USER_NAME;
+      public final static String USER_NAME = "y0tsu";
 
     public static void main(String[] args) {
         String apiUrl = "https://api.github.com/users/";
